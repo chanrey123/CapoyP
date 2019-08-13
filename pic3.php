e7527dae3511ee0f09ae7781187f4a375e8377fa
chanrey123 <53500006+chanrey123@users.noreply.github.com>
2019-08-13 10:23:14 +0800

    Create pic3.php

---
+++ pic3.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html?
+<html lang in="en">
+<head><title>P3</title></head>
+<body>
+<center><h1>CAPOYC Photo List</h1>
+<img src="pic3.jpg"></center>
+<a href="index.php"><b>BAKC TO HOME</b></a>
+</body>
+</html>
