2e60c4a42a6473a18c36379641db984ad434878f
chanrey123 <53500006+chanrey123@users.noreply.github.com>
2019-08-13 09:54:06 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+<head><title>"PHOTO ASSIGNMENT"</title></head>
+<body>
+<h1><center>CAPOYC Photo List</center></h1>
+<h3><center>LIST OF PHOTOS</center></h3>
+<center><a href="pic1.php">Photo 1</a></center>
+<center><a href="pic2.php">Photo 2</a></center>
+<center><a href="pic3.php">Photp 3</a></center>
+</head>
+</body>
+</html>
