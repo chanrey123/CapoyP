e7ab0ef158407e4dc2fbdf225474a4bc868262d9
chanrey123 <53500006+chanrey123@users.noreply.github.com>
2019-08-13 10:41:05 +0800

    Update pic3.php

--- pic3.php
+++ pic3.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html?
 <html lang in="en">
-<head><title>P3</title></head>
+<head><title>Chevrolet Camaro</title></head>
 <body>
 <center><h1>CAPOYC Photo List</h1>
 <img src="pic3.jpg"></center>
