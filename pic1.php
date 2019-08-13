228c84dc06e150cb68a10e28d6c50a6468d3e8f1
chanrey123 <53500006+chanrey123@users.noreply.github.com>
2019-08-13 10:38:19 +0800

    Update pic1.php

--- pic1.php
+++ pic1.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-<head><title>P1</title></head>
+<head><title>Lamborghini</title></head>
 <body>
 <center><h1>CAPOYC Photo List</h1></center>
-<center>img src="pic1.jpg"></center?
+<center><img src="pic1.jpg"></center?
 <a href="index.php"><b>BACK TO HOME</b></a>
 <a href="pic2.php" style="float: right;"<b>NEXT PHOTO</b></a>
 </body>
