544c301f650432893ef7372691b19635c88ffa1e
chanrey123 <53500006+chanrey123@users.noreply.github.com>
2019-08-13 10:09:41 +0800

    Create pic1.php

---
+++ pic1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head><title>P1</title></head>
+<body>
+<center><h1>CAPOYC Photo List</h1></center>
+<center>img src="pic1.jpg"></center?
+<a href="index.php"><b>BACK TO HOME</b></a>
+<a href="pic2.php" style="float: right;"<b>NEXT PHOTO</b></a>
+</body>
+</html>
