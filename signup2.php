fa33cf131d3df5fd2f4ed35912642917d28ce23b
chanrey123 <53500006+chanrey123@users.noreply.github.com>
2019-08-23 20:08:35 +0700

    Update signup2.php

--- signup2.php
+++ signup2.php
@@ -4,8 +4,8 @@
 <title>CONGRATULATIONS</title>
 </head>
 <body align="center">
-<h1> Success! </h1>
-<h1> You may now login your account.</h1>
+<h1> Congratulations! </h1>
+<h1> Log in toyr account safely </h1>
 <p> Go back to
 <a href="index.php">login page.</a>
 </body>
