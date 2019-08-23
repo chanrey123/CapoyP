548f3f738cb8fa87b7e4792bb1db195b05205071
chanrey123 <53500006+chanrey123@users.noreply.github.com>
2019-08-23 20:05:23 +0700

    Create signup2.php

---
+++ signup2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>CONGRATULATIONS</title>
+</head>
+<body align="center">
+<h1> Success! </h1>
+<h1> You may now login your account.</h1>
+<p> Go back to
+<a href="index.php">login page.</a>
+</body>
+</html>
+
