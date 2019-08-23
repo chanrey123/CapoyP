98ff172914f20abc76840e2a81d03669ee97100e
chanrey123 <53500006+chanrey123@users.noreply.github.com>
2019-08-23 19:40:38 +0700

    Update photo.php

--- photo.php
+++ photo.php
@@ -6,8 +6,10 @@
 <h3><center>LIST OF PHOTOS</center></h3>
 <center><a href="pic1.php">Photo 1</a></center>
 <center><a href="pic2.php">Photo 2</a></center>
-<center><a href="pic3.php">Photp 3</a></center>
+<center><a href="pic3.php">Photp 3</a></center>\
+  <center>
   <a href="index.php">Log out</a>
+  </center>
 </head>
 </body>
 </html>
